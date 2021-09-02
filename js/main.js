eea5cc5ee12d2a1d879d59925d66b28bb3b50108
afsumeem <afsumeem@gmail.com>
2021-09-02 21:40:13 +0600

    final

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-const searchInput = document.getElementById('search-book');
 const searchBtn = document.getElementById('input-button');
 const bookContainer = document.getElementById('book-container');
 const errorMessage = document.getElementById('error-message');
@@ -6,7 +5,9 @@ const totalBook = document.getElementById('total-result');
 
 
 //Button handler and load data
+
 searchBtn.addEventListener('click', () => {
+    const searchInput = document.getElementById('search-book');
     const search = searchInput.value;
 
     //clear search bar
@@ -40,7 +41,6 @@ searchBtn.addEventListener('click', () => {
 //Display result
 const displayResult = (showBooks) => {
     const books = showBooks.docs;
-    books.length = 30;
 
     //error handling
     if (books.length === 0) {
@@ -57,6 +57,7 @@ const displayResult = (showBooks) => {
         </div
     `;
 
+    books.length = 30;
 
     //books container 
     books.forEach(book => {
@@ -68,18 +69,20 @@ const displayResult = (showBooks) => {
         div.classList.add('col');
         div.innerHTML = `
             <div class="card h-100">
-                <img src="${displayimg}" class="card-img-top img-fluid h-50 w-75 mt-4 mx-auto" alt="${book.title}">
+                <img src="${displayimg}" class="card-img-top border img-fluid h-50 w-75 my-4 mx-auto" alt="${book.title}">
                 <div class="card-body">
                     <h5 class="card-title mb-3">${book.title}</h5>
                     <p><span class="fw-bold">Author:</span> ${book.author_name}</p>
-                    <p><span class="fw-bold">Publisher:</span> ${book.publisher.slice(0, 20)}</p>
+                    <p><span class="fw-bold">Publisher:</span> ${book.publisher.slice(0, 5)}</p>
                 </div >
                 <div class="card-footer">
-                <small><span class="fw-bold">First Publish:</span> ${book.first_publish_year}</small>
+                <small><span class="fw-bold">Publish Year:</span> ${book.first_publish_year}</small>
                 </div >
             </div >
-            `;
+    `;
         bookContainer.appendChild(div);
     });
 }
+
+
 
